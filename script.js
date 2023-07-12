c6decd9a4428ced16153068fc33ff0a5d8578e04
Nelson Ajibise <139356061+nelson780@users.noreply.github.com>
2023-07-12 09:15:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('body nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('body nav a [href*=' + id + ']').classList.add ('active';)
+            });
+
+
+        };
+    })
+}
